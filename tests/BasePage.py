87cdd65ab26bb14cadab77288d5ff57b583a14f9
Marco-Polo-coding <dawtardeiestiernogalvan@gmail.com>
2025-12-16 17:18:24 +0100

    Push to prod: the sequel :P

--- tests/BasePage.py
+++ tests/BasePage.py
@@ -1,12 +1,23 @@
 from drivers.webdrivers.WebdriverFactory import WebdriverFactory
+from drivers.webdrivers.AuxiliaryMethods import AuxiliaryMethods as AM
 
-driver = WebdriverFactory.create_from_properties()
-base_url = WebdriverFactory.get_param("url")  # o "base_url" si lo cambias
+class BasePage:
+    def __init__(self, driver=None):
+        self.driver = driver or WebdriverFactory.create_from_properties()
+        self.base_url = WebdriverFactory.get_param("url")
 
+    def open_home(self):
+        self.driver.get(self.base_url)
+        return self
 
-def main():
-    try:
-        driver.get(base_url)
-        
-    finally:
-        driver.quit()
+    def go_to(self, path: str):
+        # path tipo "/login"
+        url = self.base_url.rstrip("/") + "/" + path.lstrip("/")
+        self.driver.get(url)
+        return self
+
+    # Opcional: exponer AM para que tus Pages hagan self.AM.click(...)
+    AM = AM
+
+    def quit(self):
+        self.driver.quit()
